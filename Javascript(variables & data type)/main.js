18cb66d9df12388caabdba5a7a87aef429d6df92
Maryam Anwer <162865221+Maryamrind@users.noreply.github.com>
2024-12-01 11:35:48 -0800

    Add files via upload

---
+++ Javascript(variables & data type)/main.js
@@ -0,0 +1,97 @@
+// Primitive Data Types
+
+// Number
+
+// example # 1 
+
+let viewers =25000;
+
+console.log(viewers);
+
+
+// Example # 2
+
+let subscribers =9999;
+subscribers = 10000;
+ subscribers = 10000+500;
+ console.log(subscribers);
+// String
+
+// Example # 1
+
+let user = "Ahsan";
+console.log(user);
+
+// Example # 2
+
+let address = "Karachi, Pakistan";
+console.log(address);
+
+//  Example # 3
+
+let age = "1234";
+console.log(age+567);
+
+//Boolean 
+
+// Example # 1
+
+let isStudent = true;
+console.log(isStudent)
+
+//Example # 2;
+
+let isFollowing = false;
+console.log(isFollowing);
+
+//Null
+
+//Example # 1
+
+let student=null;
+console.log(student);
+
+//Example # 2
+
+let myProfile = null;
+console.log(myProfile);
+
+// undefined
+
+// Example # 1
+
+let school;
+console.log(school);
+
+
+//Example # 2
+
+let copy;
+console.log(copy);
+
+//Non-Primitive Data Types
+
+//Objects
+
+// Example # 1
+// Object of Product
+
+const product={
+    title:"Ball Pen",
+    rating: 4,
+    offer:5,
+    Price:270,
+};
+console.log(product);
+
+
+// Example # 2
+//Object of Profile
+
+const profile={
+    username: "Maryam",
+    isFollow:true,
+    follower:87,
+    following:91
+}
+console.log(profile["follower"]);
